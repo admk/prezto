15159e8e55dccc656344693d1f68d02695bdec18
Xitong Gao <gxtfmx@gmail.com>
2013-04-17 15:36:11 +0100

    Allow custom prezto path

--- init.zsh
+++ init.zsh
@@ -27,11 +27,16 @@ function pmodload {
   local pmodule
   local pfunction_glob='^([_.]*|prompt_*_setup|README*)(.N:t)'
 
+  # Prezto directory
+  if [[ ! -d $PREZTO ]]; then
+      PREZTO="${ZDOTDIR:-$HOME}/.zprezto"
+  fi
+
   # $argv is overridden in the anonymous function.
   pmodules=("$argv[@]")
 
   # Add functions to $fpath.
-  fpath=(${pmodules:+${ZDOTDIR:-$HOME}/.zprezto/modules/${^pmodules}/functions(/FN)} $fpath)
+  fpath=(${pmodules:+$PREZTO/modules/${^pmodules}/functions(/FN)} $fpath)
 
   function {
     local pfunction
@@ -40,7 +45,7 @@ function pmodload {
     setopt LOCAL_OPTIONS EXTENDED_GLOB
 
     # Load Prezto functions.
-    for pfunction in ${ZDOTDIR:-$HOME}/.zprezto/modules/${^pmodules}/functions/$~pfunction_glob; do
+    for pfunction in $PREZTO/modules/${^pmodules}/functions/$~pfunction_glob; do
       autoload -Uz "$pfunction"
     done
   }
@@ -49,19 +54,19 @@ function pmodload {
   for pmodule in "$pmodules[@]"; do
     if zstyle -t ":prezto:module:$pmodule" loaded 'yes' 'no'; then
       continue
-    elif [[ ! -d "${ZDOTDIR:-$HOME}/.zprezto/modules/$pmodule" ]]; then
+    elif [[ ! -d "$PREZTO/modules/$pmodule" ]]; then
       print "$0: no such module: $pmodule" >&2
       continue
     else
-      if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/modules/$pmodule/init.zsh" ]]; then
-        source "${ZDOTDIR:-$HOME}/.zprezto/modules/$pmodule/init.zsh"
+      if [[ -s "$PREZTO/modules/$pmodule/init.zsh" ]]; then
+        source "$PREZTO/modules/$pmodule/init.zsh"
       fi
 
       if (( $? == 0 )); then
         zstyle ":prezto:module:$pmodule" loaded 'yes'
       else
         # Remove the $fpath entry.
-        fpath[(r)${ZDOTDIR:-$HOME}/.zprezto/modules/${pmodule}/functions]=()
+        fpath[(r)$PREZTO/modules/${pmodule}/functions]=()
 
         function {
           local pfunction
@@ -71,7 +76,7 @@ function pmodload {
           setopt LOCAL_OPTIONS EXTENDED_GLOB
 
           # Unload Prezto functions.
-          for pfunction in ${ZDOTDIR:-$HOME}/.zprezto/modules/$pmodule/functions/$~pfunction_glob; do
+          for pfunction in $PREZTO/modules/$pmodule/functions/$~pfunction_glob; do
             unfunction "$pfunction"
           done
         }
